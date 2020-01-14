132c86908fc3c9524544764c0f7885b85abae890
youth2ee <sprirealee@gmail.com>
2020-01-14 19:42:59 +0900

    200114

--- src/main/java/com/airline/a1/imPay/ImPayController.java
+++ src/main/java/com/airline/a1/imPay/ImPayController.java
@@ -16,10 +16,11 @@ public class ImPayController {
 
 	
 	@RequestMapping("imPayList")
-	@ResponseBody
 	public Model imPayList(ImPayVO imPayVO, Model model) {
-		imPayVO.setName("test결제");
-		imPayVO.setAmount(100);
+		
+		System.out.println(imPayVO.getName());
+		System.out.println(imPayVO.getAmount());
+		
 		MembersVO membersVO = new MembersVO();
 		membersVO.setName("이가영");
 		membersVO.setEmail("sprirealee@gmail.com");
@@ -28,14 +29,25 @@ public class ImPayController {
 		
 		model.addAttribute("VO", imPayVO);
 		
-		
 		return model;
 	}
 	
 	
 	@RequestMapping("imPayComplete")
-	public void imPayComplete() {
-		
+	public void imPayComplete(ImPayResultVO imPayResultVO) {
+		System.out.println(imPayResultVO.getSuccess());
+		System.out.println(imPayResultVO.getImp_uid());
+		System.out.println(imPayResultVO.getMerchant_uid());
+		System.out.println(imPayResultVO.getPay_method());
+		System.out.println(imPayResultVO.getPaid_amount());
+		System.out.println(imPayResultVO.getStatus());
+		System.out.println(imPayResultVO.getName());
+		System.out.println(imPayResultVO.getPg_provider());
+		System.out.println(imPayResultVO.getPg_tid());
+		System.out.println(imPayResultVO.getPaid_at());
+		System.out.println(imPayResultVO.getReceipt_url());
+		System.out.println(imPayResultVO.getApply_num());
+
 	}
 	
 }
