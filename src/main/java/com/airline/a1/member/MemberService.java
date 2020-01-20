3c24e018ae8bf770cd4b7059bf748985b042013c
daze123 <daze123@hanmail.net>
2020-01-20 20:01:36 +0900

    .

--- src/main/java/com/airline/a1/member/MemberService.java
+++ src/main/java/com/airline/a1/member/MemberService.java
@@ -1,8 +1,21 @@
 package com.airline.a1.member;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-
+	
+	// 회원번호 생성
+	public String mkMemberNum() throws Exception {
+		Random rand = new Random();
+		String memberNumber1 = String.format("%03d%n", rand.nextInt(1000)).replace("\r\n", "");
+		String memberNumber2 = String.format("%03d%n", rand.nextInt(1000)).replace("\r\n", "");
+		String memberNumber3 = String.format("%03d%n", rand.nextInt(1000)).replace("\r\n", "");
+		String memberNumber = memberNumber1 + " " + memberNumber2 + " " + memberNumber3; // 띄어쓰기 있음 (아시아나 형식)
+		// String memberNumber = memberNumber1 + memberNumber2 + memberNumber3; // 띄어쓰기 없음  (편의상)
+		System.out.println("회원번호 : " + memberNumber);
+		return memberNumber;
+	}
 }
--- src/test/java/com/airline/a1/AirlineApplicationTests.java
+++ src/test/java/com/airline/a1/AirlineApplicationTests.java
@@ -24,6 +24,7 @@ import org.w3c.dom.NodeList;
 import com.airline.a1.api.AirportDataMapper;
 
 import com.airline.a1.api.BusVO;
+import com.airline.a1.booking.BookingTicketVO;
 import com.airline.a1.booking.FlightDataVO;
 
 @SpringBootTest
@@ -38,6 +39,20 @@ class AirlineApplicationTests {
 	@Autowired
 	private BusVO busVO;
 
+	@Test
+		void apiTest7() throws Exception {
+		System.out.println("test");
+			Random rand = new Random();
+			String flightName = /*bookingTicketVO.getDepInfo().getVihicleId()*/ "HGG/1301";
+			if(flightName.contains("/"))
+				flightName = flightName.replace("/","");
+			String filghtName1 = String.format("%03d%n", rand.nextInt(1000)).replace("\r\n", "");
+			char filghtName2 = (char) ((int) (Math.random() * 26) + 65);
+			flightName = flightName +Character.toString(filghtName2)+ filghtName1 ;
+			System.out.println("예약번호(개인) : " + flightName);
+		}
+
+	
 	@Test
 	void apiTest6() throws Exception {
 		Random rand = new Random();
@@ -177,7 +192,7 @@ class AirlineApplicationTests {
 	}
 
 
-	 @Test
+	// @Test
 	void apiTest3() throws Exception {
 		// 항공정보 + 운임
 		BufferedReader br = null;
