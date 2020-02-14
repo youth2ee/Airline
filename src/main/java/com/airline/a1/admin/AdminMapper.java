abde14abf71a73153f87937cd31c7ad8d57ebeb5
Changkyu22 <55524640+Changkyu22@users.noreply.github.com>
2020-02-14 16:53:47 +0900

    Revert "20.2.13."
    This reverts commit 1459604c9402320840cfc3c6b843d4d2b451e5d1.

--- src/main/java/com/airline/a1/admin/AdminMapper.java
+++ src/main/java/com/airline/a1/admin/AdminMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.airline.a1.booking.FlightDataVO;
-import com.airline.a1.limo.LimoVO;
 
 
 @Repository
@@ -16,10 +15,5 @@ public interface AdminMapper {
 	public List<AirlineVO> airlineList() throws Exception;
 	public List<FlightDataVO> airpartList(FlightDataVO flightDataVO) throws Exception;
 	public List<FlightDataVO> airallList() throws Exception;
-	public Integer limoSum(String airport)throws Exception;
-	public Integer limoTotalSum()throws Exception;
-	public Integer limoTotalPerson(String airport)throws Exception;
-	public Integer personSum()throws Exception;
-	public Integer todaySum(String airport)throws Exception;
-	public Integer limoTodaySum()throws Exception;
+	
 }
